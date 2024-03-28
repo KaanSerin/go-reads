7a0f92a8f91bf806b4aaff317a602c247cd18dcb
KaanSerin <kaanserin113@gmail.com>
2024-03-28 14:00:16 +0300

    feat: allow optional db connection with url

--- internal/database/database.go
+++ internal/database/database.go
@@ -21,13 +21,21 @@ type PostgresqlStorage struct {
 }
 
 func NewPostgresStorage() (*PostgresqlStorage, error) {
+	dbUrl := os.Getenv("DB_URL")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPass := os.Getenv("DB_PASSWORD")
 
-	connStr := fmt.Sprintf("host=%s port=%s sslmode=disable dbname=%s user=%s password=%s", dbHost, dbPort, dbName, dbUser, dbPass)
+	var connStr string
+	if dbUrl != "" {
+		connStr = dbUrl
+		fmt.Println("Trying DB connection using ", dbUrl)
+	} else {
+		connStr = fmt.Sprintf("host=%s port=%s sslmode=disable dbname=%s user=%s password=%s", dbHost, dbPort, dbName, dbUser, dbPass)
+	}
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
