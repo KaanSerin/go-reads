48d747db9feaf2812fc6dfb617167c037c84057b
KaanSerin <kaanserin113@gmail.com>
2024-03-28 22:04:46 +0300

    feat: register logger and auth route

--- internal/api/router.go
+++ internal/api/router.go
@@ -4,11 +4,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/kaanserin/go-reads/internal/auth"
 	users "github.com/kaanserin/go-reads/internal/users"
 )
 
 func CreateNewRouter() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
 
 	// Register routes here
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +21,8 @@ func CreateNewRouter() *chi.Mux {
 
 	// Users routes
 	r.Route("/users", users.GetUsersRouter)
+
+	r.Route("/auth", auth.GetAuthRouter)
 
 	return r
 }
