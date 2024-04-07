6680327508bbea726ebedf8e7710efb026501482
KaanSerin <kaanserin113@gmail.com>
2024-04-07 17:44:34 +0300

    fix: add default value to created_at

--- init.sql
+++ init.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS users (
     last_name VARCHAR(50),
     email VARCHAR(320),
     password TEXT,
-    created_at TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
