4e22a62579b37ffc656cb5a13ee22f5ca7e1b70a
KaanSerin <kaanserin113@gmail.com>
2024-04-07 16:44:17 +0300

    fix: connect using db url

--- internal/database/database.go
+++ internal/database/database.go
@@ -122,21 +122,7 @@ func (storage *PostgresqlStorage) DeleteUserById(id int) error {
 
 func NewPostgresStorage() (*PostgresqlStorage, error) {
 	dbUrl := os.Getenv("DB_URL")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USERNAME")
-	dbPass := os.Getenv("DB_PASSWORD")
-
-	var connStr string
-	if dbUrl != "" {
-		connStr = dbUrl
-		fmt.Println("Trying DB connection using ", dbUrl)
-	} else {
-		connStr = fmt.Sprintf("host=%s port=%s sslmode=disable dbname=%s user=%s password=%s", dbHost, dbPort, dbName, dbUser, dbPass)
-	}
-
-	db, err := sql.Open("postgres", connStr)
+	db, err := sql.Open("postgres", dbUrl)
 	if err != nil {
 		return nil, err
 	}
