4d5cfc2a39e602dd51c85478094be290d270bebd
KaanSerin <kaanserin113@gmail.com>
2024-04-08 13:04:20 +0300

    feat: implement sign_in route handler

--- internal/auth/auth_handler.go
+++ internal/auth/auth_handler.go
@@ -1,8 +1,10 @@
 package auth
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"time"
 
@@ -32,6 +34,7 @@ type AuthUserResponse struct {
 func AddAuthRoutes(c *gin.Engine) {
 	router := c.Group("/auth")
 	router.POST("/sign_up", makeHandlerFunc(signUpHandler))
+	router.POST("/sign_in", makeHandlerFunc(signInHandler))
 }
 
 // Handlers
@@ -84,6 +87,60 @@ func hashPassword(password string) (string, error) {
 	}
 
 	return string(hashedPassword), nil
+}
+
+type SignInDto struct {
+	Email    string `validate:"nonzero"`
+	Password string `validate:"nonzero"`
+}
+
+func signInHandler(c *gin.Context) error {
+	var signIn SignInDto
+	if err := json.NewDecoder(c.Request.Body).Decode(&signIn); err != nil {
+		return err
+	}
+
+	if err := validator.Validate(signIn); err != nil {
+		return err
+	}
+
+	storage, err := database.GetPgStorageFromRequest(c.Request)
+	if err != nil {
+		return err
+	}
+
+	user, err := storage.GetUserByEmail(signIn.Email)
+	if err == sql.ErrNoRows {
+		c.JSON(http.StatusUnauthorized, &utils.CustomError{
+			Message: "Invalid email or password",
+		})
+
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(signIn.Password))
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusUnauthorized, &utils.CustomError{
+			Message: "Invalid email or password",
+		})
+		return nil
+	}
+
+	accessToken, err := getAccessTokenStringForUser(user)
+	if err != nil {
+		return err
+	}
+
+	user.Password = ""
+
+	c.JSON(http.StatusOK, AuthUserResponse{
+		User:        user,
+		AccessToken: accessToken,
+	})
+	return nil
 }
 
 func getAccessTokenStringForUser(user *database.User) (string, error) {
